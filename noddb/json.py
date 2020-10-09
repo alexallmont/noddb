5f0fe08a701dd167392cf3483e527eb1f1aa5288
Alex Allmont <info@alexallmont.com>
2020-10-09 16:18:02 +0100

    Add noddb importer

--- noddb/json.py
+++ noddb/json.py
@@ -6,10 +6,17 @@ from .value import InputValue, OutputValue
 from .visitor import Visitor
 
 
+class ExportException(Exception):
+    pass
+
+
+class ImportException(Exception):
+    pass
+
+
 class JsonRegistry:
-    def __init__(self, node_types=[], value_types=[]):
-        self.node_dict = {typ.__name__: typ for typ in node_types}
-        self.value_dict = {typ.__name__: typ for typ in value_types + standard_value_types()}
+    def __init__(self, custom_types: List[NodeBase] = []):
+        self.type_dict = {typ.__name__: typ for typ in custom_types + standard_value_types()}
 
     def export_json(self, nodes: List[NodeBase]):
         # Allow roots to be a single node or a list of nodes
@@ -21,9 +28,88 @@ class JsonRegistry:
             node.visit(export)
         return export.to_json()
 
+    def import_json(self, json_dict: dict) -> List[NodeBase]:
+        if not all(key in json_dict for key in ['nodes', 'values', 'sources']):
+            raise ImportException('Expecting nodes, values and sources entries at root')
 
-class ExportException(Exception):
-    pass
+        if not all(type(value) == dict for value in json_dict.values()):
+            raise ImportException('Expecting dict type for nodes, values and sources')
+
+        # Recursively instantiate all nodes by type
+        loader = NodesLoader(self, json_dict['nodes'])
+
+        # Set values from keys in values dict
+        for path, value in json_dict['values'].items():
+            if path not in loader.node_dict:
+                raise ImportException(f"Invalid path '{path}' when loading values")
+            loader.node_dict[path].set_value(value)
+
+        # Connect values from keys in sources dict
+        for dst, src in json_dict['sources'].items():
+            if dst not in loader.node_dict:
+                raise ImportException(f"Invalid input destination '{dst}' when loading sources")
+            if src not in loader.node_dict:
+                raise ImportException(f"Invalid output source '{src}' when loading sources")
+            dst_value = loader.node_dict[dst]
+            src_value = loader.node_dict[src]
+            dst_value << src_value
+
+        return loader.nodes
+
+
+class NodesLoader:
+    def __init__(self, registry: JsonRegistry, json_dict: dict):
+        self.registry = registry
+        self.nodes = []
+        self.node_dict = {}
+
+        self._node_stack = []
+        self._import_dict(json_dict)
+
+    def _import_dict(self, json_dict: dict):
+        for name, json_obj in json_dict.items():
+            self._import_obj(name, json_obj)
+
+    def _import_list(self, node_list: list):
+        for json_obj in node_list:
+            self._import_obj(None, json_obj)
+
+    def _import_obj(self, name: str, json_obj):
+        if isinstance(json_obj, dict):
+            node = self._create_node(Node, name)
+            self._push_parent(node)
+            self._import_dict(json_obj)
+            self._pop_parent()
+        elif isinstance(json_obj, list):
+            node = self._create_node(NodeArray, name)
+            self._push_parent(node)
+            self._import_list(json_obj)
+            self._pop_parent()
+            pass
+        elif isinstance(json_obj, str):
+            typename = json_obj
+            if typename not in self.registry.type_dict:
+                raise ExportException(f"Unexpected node type '{typename}' during import")
+            type_class = self.registry.type_dict[typename]
+            self._create_node(type_class, name)
+        else:
+            raise ImportException(f'Expecting dict, list or string values for nodes, found {type(json_obj)}')
+
+    def _push_parent(self, node: NodeBase):
+        self._node_stack.append(node)
+
+    def _pop_parent(self):
+        self._node_stack.pop()
+
+    def _create_node(self, type_class: NodeBase, name=None):
+        parent = self._node_stack[-1] if self._node_stack else None
+        node = type_class(parent, name)
+        self.node_dict[node.path()] = node
+
+        # Store root nodes in returned import list
+        if not parent:
+            self.nodes.append(node)
+        return node
 
 
 class ExportVisitor(Visitor):
@@ -37,43 +123,42 @@ class ExportVisitor(Visitor):
     def on_node_enter(self, node: Node):
         if self._can_store():
             if node.is_custom():
-                if node.typename not in self.registry.node_dict:
+                if node.typename not in self.registry.type_dict:
                     raise ExportException(f"Unexpected node type '{node.typename}' during export")
 
                 # If this is a custom leaf node, then it can be serialised by typename
                 self._add_to_container(node.name, node.typename)
                 # Prevent descent into any children of custom node, e.g. values, sub-nodes
-                self._json_stack.append(None)
+                self._push_container(None)
             else:
                 # Otherwise create new dictionary for children in descent
                 node_dict = {}
                 self._add_to_container(node.name, node_dict)
-                self._json_stack.append(node_dict)
+                self._push_container(node_dict)
         else:
             # Continue preventing storage of descendants
-            self._json_stack.append(None)
+            self._push_container(None)
 
     def on_node_exit(self, node: Node):
-        self._json_stack.pop()
+        self._pop_container()
 
     def on_node_array_enter(self, node: NodeArray):
         if self._can_store():
             # Create an array type to descend into
             node_array = []
             self._add_to_container(node.name, node_array)
-            self._json_stack.append(node_array)
+            self._push_container(node_array)
         else:
             # Continue preventing storage of descendants
-            self._json_stack.append(None)
+            self._push_container(None)
 
     def on_node_array_exit(self, node: NodeArray):
-        self._json_stack.pop()
+        self._pop_container()
 
     def on_input(self, value: InputValue):
         if self._can_store():
-            if value.typename not in self.registry.value_dict:
+            if value.typename not in self.registry.type_dict:
                 raise ExportException(f"Unexpected input value type '{value.typename}' during export")
-
             self._add_to_container(value.name, value.typename)
 
         if value.is_sourced():
@@ -83,12 +168,17 @@ class ExportVisitor(Visitor):
 
     def on_output(self, value: OutputValue):
         if self._can_store():
-            if value.typename not in self.registry.value_dict:
+            if value.typename not in self.registry.type_dict:
                 raise ExportException(f"Unexpected output value type '{value.typename}' during export")
-
             self._add_to_container(value.name, value.typename)
 
         self.values[value.path()] = value.value()
+
+    def _push_container(self, json_obj):
+        self._json_stack.append(json_obj)
+
+    def _pop_container(self):
+        self._json_stack.pop()
 
     def _can_store(self):
         return self._json_stack[-1] is not None
--- tests/test_json.py
+++ tests/test_json.py
@@ -2,8 +2,9 @@ import pytest
 from pytest import approx
 
 from noddb.node import Node, NodeArray
-from noddb.json import JsonRegistry, ExportException
-from noddb.std_value import InputBool, InputInt, InputString, OutputBool, OutputInt, OutputFloat
+from noddb.json import JsonRegistry, ExportException, ImportException
+from noddb.std_value import InputBool, InputInt, InputFloat, InputString
+from noddb.std_value import OutputBool, OutputInt, OutputFloat, OutputString
 from noddb.value import InputValue, OutputValue
 
 
@@ -113,3 +114,147 @@ def test_export_interconnected():
             'extra_root.outy': 2
         }
     }
+
+
+def test_bad_import():
+    registry = JsonRegistry()
+
+    with pytest.raises(ImportException) as excinfo:
+        registry.import_json({
+            'nodes': {},
+            'values': {},
+            'tastysnacks': {}
+        })
+    assert str(excinfo.value) == 'Expecting nodes, values and sources entries at root'
+
+    with pytest.raises(ImportException) as excinfo:
+        registry.import_json({
+            'nodes': {},
+            'values': [],
+            'sources': {}
+        })
+    assert str(excinfo.value) == 'Expecting dict type for nodes, values and sources'
+
+    with pytest.raises(ImportException) as excinfo:
+        registry.import_json({
+            'nodes': {
+                'badtype': True
+            },
+            'values': {},
+            'sources': {}
+        })
+    assert str(excinfo.value) == "Expecting dict, list or string values for nodes, found <class 'bool'>"
+
+
+def test_node_import():
+    class FooNode(Node):
+        pass
+
+    registry = JsonRegistry([FooNode])
+    a_nodes = registry.import_json({
+        'nodes': {
+            'root': 'InputInt'
+        },
+        'values': {},
+        'sources': {}
+    })
+    assert len(a_nodes) == 1
+    assert type(a_nodes[0]) == InputInt
+    assert a_nodes[0].name == 'root'
+    assert a_nodes[0].value() == 0
+
+    b_nodes = registry.import_json({
+        'nodes': {
+            'root1': ['InputInt'],
+            'root2': 'FooNode'
+        },
+        'values': {},
+        'sources': {}
+    })
+    assert len(b_nodes) == 2
+    assert type(b_nodes[0]) == NodeArray
+    assert len(b_nodes[0].children) == 1
+    assert type(b_nodes[0][0]) == InputInt
+    assert type(b_nodes[1]) == FooNode
+
+    c_nodes = registry.import_json({
+        'nodes': {
+            'foo': {
+                'bar': {
+                    'a': 'FooNode',
+                    'b': 'InputBool',
+                    'c': 'OutputFloat'
+                }
+            },
+        },
+        'values': {},
+        'sources': {}
+    })
+    assert type(c_nodes[0]) == Node
+    assert c_nodes[0].name == 'foo'
+    assert type(c_nodes[0]['bar']) == Node
+    assert type(c_nodes[0]['bar']['a']) == FooNode
+    assert type(c_nodes[0]['bar']['b']) == InputBool
+    assert type(c_nodes[0]['bar']['c']) == OutputFloat
+
+
+def test_value_import_set():
+    registry = JsonRegistry()
+    nodes = registry.import_json({
+        'nodes': {
+            'a': 'OutputString'
+        },
+        'values': {
+            'a': 'stuff',
+        },
+        'sources': {}
+    })
+    assert type(nodes[0]) == OutputString
+    assert nodes[0].value() == 'stuff'
+
+    nodes = registry.import_json({
+        'nodes': {
+            'foo': {
+                'bar': [
+                    [
+                        [
+                            'OutputFloat',
+                            'InputFloat'
+                        ]
+                    ]
+                ]
+            }
+        },
+        'values': {
+            'foo.bar[0][0][0]': 3.0,
+            'foo.bar[0][0][1]': 7.0,
+        },
+        'sources': {}
+    })
+    assert type(nodes[0]['bar'][0][0][0]) == OutputFloat
+    assert type(nodes[0]['bar'][0][0][1]) == InputFloat
+    assert nodes[0]['bar'][0][0][0].value() == approx(3)
+    assert nodes[0]['bar'][0][0][1].value() == approx(7)
+
+
+def test_source_import_set():
+    registry = JsonRegistry()
+    nodes = registry.import_json({
+        'nodes': {
+            'foo': {
+                'a': 'OutputFloat'
+            },
+            'bar': {
+                'b': 'InputFloat'
+            }
+        },
+        'values': {
+            'foo.a': 6.0,
+        },
+        'sources': {
+            'bar.b': 'foo.a'
+        }
+    })
+    assert nodes[0]['a'].value() == approx(6)
+    assert nodes[1]['b'].is_sourced() is True
+    assert nodes[1]['b'].value() == approx(6)
