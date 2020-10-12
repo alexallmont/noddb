253dc6bcea9c1eb75db0c3546a633c55901f04c5
Alex Allmont <info@alexallmont.com>
2020-10-12 17:55:20 +0100

    Add method for getting a node by path

--- noddb/path.py
+++ noddb/path.py
@@ -1,4 +1,5 @@
 from typing import List, Union
+from .node import Node, NodeBase
 import re
 
 
@@ -12,9 +13,33 @@ def split_path(path: str) -> List[Union[str, int]]:
     """
     path_items = re.split('[.[]', path)
 
+    # When the first path element is an array path, e.g. "[0]", an erroneous empty string
+    # is the first item. This explicitly removes it.
+    if path_items and not path_items[0]:
+        path_items.pop(0)
+
     def int_if_possible(name):
         if name[0].isdigit():
             return int(name.strip(']'))
         return name
 
     return [int_if_possible(name) for name in path_items]
+
+
+def path_to_node(root: Union[Node, dict], path: str) -> NodeBase:
+    """
+    Get a node given root node or dict and a relative path. Note that the dict option
+    for the root is for convenience when dealing with imported files, where top-level
+    nodes are stored in a dict.
+    :param root: Node or dict at root of search
+    :param path: Path from root location to find node from
+    :return: Found node or value
+    """
+    names = split_path(path)
+
+    node = root
+    while names:
+        name_or_index = names.pop(0)
+        node = node[name_or_index]
+
+    return node
